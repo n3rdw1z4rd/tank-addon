7457507de9cad939907149f91076171e2fa3ca4a
John Wakley <jwakley74@gmail.com>
2020-10-30 16:29:13 -0400

    cleaned up TOC, added convenience functions to sdb

--- TankAddon/sdb.lua
+++ TankAddon/sdb.lua
@@ -9,14 +9,50 @@ end
 
 function sdb:log_debug(...)
     if debug then
-        print("|cff888888" .. ...)
+        print("|cff888888", ...)
     end
 end
 
 function sdb:log_info(...)
-    print("|cff00ffff" .. ...)
+    print("|cff00ffff", ...)
 end
 
 function sdb:log_error(...)
-    print("|cffff8888" .. ...)
+    print("|cffff8888", ...)
+end
+
+function sdb:log_debug_table(tbl)
+    table.foreach(tbl, function(k, v)
+        sdb:log_debug(k .. ": ", v)
+    end)
+end
+
+function sdb:count_table_pairs(tbl)
+    local count = 0
+
+    for _ in pairs(tbl) do
+        count = count + 1
+    end
+
+    return count
+end
+
+function sdb:contains(tbl, key)
+    for k in pairs(tbl) do
+        if k == key then
+            return true
+        end
+    end
+
+    return false
+end
+
+function sdb:GetOptionDefaults(options)
+    local defaults = {}
+
+    for k, v in pairs(options) do
+        defaults[k] = v.default    
+    end
+
+    return defaults
 end
