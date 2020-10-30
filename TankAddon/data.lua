1bcdbe63abed6af90f8900499e9a8320196490f7
John Wakley <jwakley74@gmail.com>
2020-10-30 16:29:34 -0400

    cleaned up data

--- TankAddon/data.lua
+++ TankAddon/data.lua
@@ -33,177 +33,55 @@ data.ClassData = {
     }
 }
 
-data.Defaults = {
-    profile = {
-        enabled = true,
-        locked = false,
-        always_show = true,
-        frame_padding = 5,
-        include_player = true,
-        includePets = false,
-        width = 80,
-        height = 30,
-        unit_padding = 5,
-        font = "Fonts/FRIZQT__.TTF",
-        font_size = 12
-    }
-}
-
-data.OptionsTable = {
-    name = "TankAddon",
-    type = "group",
-    args = {
-        general = {
-            name = "General Options",
-            type = "group",
-            order = 1,
-            args = {
-                enabled = {
-                    name = "Enabled",
-                    type = "toggle",
-                    order = 0,
-                    width = "full",
-                    get = function(i)
-                        return db.enabled
-                    end,
-                    set = function(i, v)
-                        db.enabled = v
-                        TA:OnOptionsUpdated()
-                    end
-                },
-                locked = {
-                    name = "Lock Frame Position",
-                    type = "toggle",
-                    order = 1,
-                    width = "full",
-                    get = function(i)
-                        return db.locked
-                    end,
-                    set = function(i, v)
-                        db.locked = v
-                        TA:OnOptionsUpdated()
-                    end
-                },
-                always_show = {
-                    name = "Always Show Frame",
-                    type = "toggle",
-                    order = 2,
-                    width = "full",
-                    get = function(i)
-                        return db.always_show
-                    end,
-                    set = function(i, v)
-                        db.always_show = v
-                        TA:OnOptionsUpdated()
-                    end
-                },
-                frame_padding = {
-                    name = "Unit Frame Padding",
-                    type = "range",
-                    min = 5,
-                    max = 15,
-                    step = 1,
-                    order = 3,
-                    width = "full",
-                    get = function(i)
-                        return db.frame_padding
-                    end,
-                    set = function(i, v)
-                        db.frame_padding = v
-                        TA:OnOptionsUpdated()
-                    end
-                },
-                -- include_player = {
-                --     name = "Inlcude Player",
-                --     type = "toggle",
-                --     order = 4,
-                --     width = "full",
-                --     get = function(i)
-                --         return db.include_player
-                --     end,
-                --     set = function(i, v)
-                --         db.include_player = v
-                --         TA:OnOptionsUpdated()
-                --     end
-                -- },
-                -- include_pets = {
-                --     name = "Inlcude Pets",
-                --     type = "toggle",
-                --     order = 5,
-                --     width = "full",
-                --     get = function(i)
-                --         return db.include_pets
-                --     end,
-                --     set = function(i, v)
-                --         db.include_pets = v
-                --         TA:OnOptionsUpdated()
-                --     end
-                -- },
-                width = {
-                    name = "Unit Width",
-                    type = "range",
-                    min = 50,
-                    max = 120,
-                    step = 1,
-                    order = 6,
-                    width = "full",
-                    get = function(i)
-                        return db.width
-                    end,
-                    set = function(i, v)
-                        db.width = v
-                        TA:OnOptionsUpdated()
-                    end
-                },
-                height = {
-                    name = "Unit Height",
-                    type = "range",
-                    min = 15,
-                    max = 50,
-                    step = 1,
-                    order = 7,
-                    width = "full",
-                    get = function(i)
-                        return db.height
-                    end,
-                    set = function(i, v)
-                        db.height = v
-                        TA:OnOptionsUpdated()
-                    end
-                },
-                unit_padding = {
-                    name = "Unit Padding",
-                    type = "range",
-                    min = 0,
-                    max = 10,
-                    step = 1,
-                    order = 8,
-                    width = "full",
-                    get = function(i)
-                        return db.unit_padding
-                    end,
-                    set = function(i, v)
-                        db.unit_padding = v
-                        TA:OnOptionsUpdated()
-                    end
-                },
-                font_size = {
-                    name = "Unit Font Size",
-                    type = "range",
-                    min = 8,
-                    max = 18,
-                    step = 2,
-                    order = 9,
-                    width = "full",
-                    get = function(i)
-                        return db.font_size
-                    end,
-                    set = function(i, v)
-                        db.font_size = v
-                        TA:OnOptionsUpdated()
-                    end
-                }
-            }
-        }
+data.Options = {
+    enabled = {
+        type = "boolean",
+        default = true
+    },
+    locked = {
+        type = "boolean",
+        default = false
+    },
+    always_show = {
+        type = "boolean",
+        default = true
+    },
+    frame_padding = {
+        type = "number",
+        min = 5,
+        max = 15,
+        step = 1,
+        default = 5
+    },
+    -- include_player = true,
+    -- includePets = false,
+    width = {
+        type = "number",
+        min = 50,
+        max = 120,
+        step = 1,
+        default = 80
+    },
+    height = {
+        type = "number",
+        min = 15,
+        max = 50,
+        step = 1,
+        default = 30
+    },
+    unit_padding = {
+        type = "number",
+        min = 0,
+        max = 10,
+        step = 1,
+        default = 5
+    },
+    -- font = "Fonts/FRIZQT__.TTF",
+    font_size = {
+        type = "number",
+        min = 8,
+        max = 18,
+        step = 2,
+        default = 12
     }
 }
