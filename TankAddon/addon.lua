d640516bfe4270ad4fb3945376491814530a2184
John Wakley <jwakley74@gmail.com>
2020-10-30 20:10:07 -0400

    added remaining support functions

--- TankAddon/addon.lua
+++ TankAddon/addon.lua
@@ -152,141 +152,419 @@ addon:SetScript("OnEvent", function(self, event, ...)
 end)
 
 -- addon functions:
--- function self:SetupOptions()
--- end
+function addon:SetupOptions()
+    sdb:log_debug("SetupOptions")
 
--- function self:CreateFrames()
---     sdb:log_debug("CreateFrames")
+    -- TODO: setup InterfaceOptionsPanel by parsing data.Options
+end
 
---     maxWidth = ((db.width + (db.unit_padding)) * unitFrameColumnCount) - db.unit_padding
---     maxHeight = ((db.height + (db.unit_padding)) * unitFrameColumnCount) - db.unit_padding
+function addon:CreateFrames()
+    sdb:log_debug("CreateFrames")
 
---     -- used fix provided by the_notorious_thug
---     self.GroupFrame = CreateFrame("Frame", "TankAddonGroupFrame", UIParent, BackdropTemplateMixin and "BackdropTemplate")
+    maxWidth = ((db.width + (db.unit_padding)) * unitFrameColumnCount) - db.unit_padding
+    maxHeight = ((db.height + (db.unit_padding)) * unitFrameColumnCount) - db.unit_padding
+    sdb:log_debug("maxWidth: ", maxWidth, ", maxHeight: ", maxHeight)
 
---     self.GroupFrame:SetFrameStrata("MEDIUM")
---     self.GroupFrame:SetMovable(true)
---     self.GroupFrame:EnableMouse(true)
---     self.GroupFrame:SetBackdrop({ bgFile = "Interface/Tooltips/UI-Tooltip-Background" })
---     self.GroupFrame:SetBackdropColor(0, 0, 0, 0.8)
---     self.GroupFrame:ClearAllPoints()
---     self.GroupFrame:SetPoint("CENTER", UIParent, "BOTTOMLEFT", 1920 / 2, 1080 / 2)
-    
---     if not db.always_show then
---         self.GroupFrame:Hide()
---     end
+    self.GroupFrame =
+        CreateFrame("Frame", "TankAddonGroupFrame", UIParent, BackdropTemplateMixin and "BackdropTemplate")
 
---     self.GroupFrame:RegisterForDrag("LeftButton")
+    self.GroupFrame:SetFrameStrata("MEDIUM")
+    self.GroupFrame:SetMovable(true)
+    self.GroupFrame:EnableMouse(true)
+    self.GroupFrame:SetBackdrop({
+        bgFile = "Interface/Tooltips/UI-Tooltip-Background"
+    })
+    self.GroupFrame:SetBackdropColor(0, 0, 0, 0.8)
+    self.GroupFrame:ClearAllPoints()
+    self.GroupFrame:SetPoint("CENTER", UIParent, "BOTTOMLEFT", 1920 / 2, 1080 / 2)
 
---     self.GroupFrame:SetScript("OnDragStart", function(self)
---         if not db.locked then
---             self:StartMoving()
---         end
---     end)
+    if not db.always_show then
+        self.GroupFrame:Hide()
+    end
 
---     self.GroupFrame:SetScript("OnDragStop", function(self)
---         if not db.locked then
---             self:StopMovingOrSizing()
---         end
---     end)
+    self.GroupFrame:RegisterForDrag("LeftButton")
 
---     function self.GroupFrame:GetUnitFrame(name)
---         for _, child in ipairs({self:GetChildren()}) do
---             if child:GetName() == name then
---                 return child
---             end
---         end
+    self.GroupFrame:SetScript("OnDragStart", function(self)
+        if not db.locked then
+            self:StartMoving()
+        end
+    end)
 
---         return nil
---     end
-    
---     function self.GroupFrame:GetUnitFrameForUnit(unit)
---         for _, child in ipairs({self:GetChildren()}) do
---             if child.unit == unit then
---                 return child
---             end
---         end
+    self.GroupFrame:SetScript("OnDragStop", function(self)
+        if not db.locked then
+            self:StopMovingOrSizing()
+        end
+    end)
 
---         return nil
---     end
+    function self.GroupFrame:GetUnitFrame(name)
+        for _, child in ipairs({self:GetChildren()}) do
+            if child:GetName() == name then
+                return child
+            end
+        end
 
---     function self.GroupFrame:UpdateThreatForUnit(unit, threatPct)
---         local child = self:GetUnitFrameForUnit(unit)
---         if child then
---             child:SetThreatPercent(threatPct)
---         else
---             sdb:log_error("UpdateThreatForUnit nil child for unit:", unit, ", threatPct:", threatPct)
---         end
---     end
+        return nil
+    end
 
---     function self.GroupFrame:ResetUnitFramesThreat()
---         for _, child in ipairs({self:GetChildren()}) do
---             child:SetThreatPercent(0)
---         end
---     end
+    function self.GroupFrame:GetUnitFrameForUnit(unit)
+        for _, child in ipairs({self:GetChildren()}) do
+            if child.unit == unit then
+                return child
+            end
+        end
 
---     function self.GroupFrame:ResetUnitFrames()
---         for _, child in ipairs({self:GetChildren()}) do
---             child.unit = nil
---             child.text:SetText(nil)
---             child:Hide()
---         end
---     end
+        return nil
+    end
 
---     local currentUnitOffsetX = db.frame_padding
---     local currentUnitOffsetY = db.frame_padding
+    function self.GroupFrame:UpdateThreatForUnit(unit, threatPct)
+        local child = self:GetUnitFrameForUnit(unit)
+        if child then
+            child:SetThreatPercent(threatPct)
+        else
+            sdb:log_error("UpdateThreatForUnit nil child for unit:", unit, ", threatPct:", threatPct)
+        end
+    end
 
---     for i = 1, maxUnitFrames do
---         -- used fix provided by the_notorious_thug
---         local button = CreateFrame("Button", format("UnitFrame%d", i), self.GroupFrame, BackdropTemplateMixin and "BackdropTemplate")
---         button:SetWidth(db.width)
---         button:SetHeight(db.height)
---         button:SetBackdrop({ bgFile = "Interface/Tooltips/UI-Tooltip-Background" })
---         button:SetPoint("BOTTOMLEFT", parent, currentUnitOffsetX, currentUnitOffsetY)
---         button.unit = nil
+    function self.GroupFrame:ResetUnitFramesThreat()
+        for _, child in ipairs({self:GetChildren()}) do
+            child:SetThreatPercent(0)
+        end
+    end
 
---         button.texture = button:CreateTexture(nil, "PARENT")
---         button.texture:SetColorTexture(1, 1, 1, 1)
---         button.texture:SetAllPoints(button)
---         button.texture:SetGradientAlpha("VERTICAL", 1, 0, 0, 0, 0, 0, 0, 0)
+    function self.GroupFrame:ResetUnitFrames()
+        for _, child in ipairs({self:GetChildren()}) do
+            child.unit = nil
+            child.text:SetText(nil)
+            child:Hide()
+        end
+    end
 
---         button.text = button:CreateFontString(nil, "ARTWORK")
---         button.text:SetFont(db.font, db.font_size)
---         button.text:SetPoint("CENTER", button, "CENTER")
+    local currentUnitOffsetX = db.frame_padding
+    local currentUnitOffsetY = db.frame_padding
 
---         button.badge = button:CreateTexture(nil, "PARENT")
---         button.badge:SetSize(20, 20)
---         button.badge:SetTexture(2202478)
---         button.badge:SetPoint("TOPLEFT", -5, 5)
+    for i = 1, maxUnitFrames do
+        -- local button = CreateFrame("Button", format("UnitFrame%d", i), self.GroupFrame, "SecureActionButtonTemplate")
+        local button = CreateFrame("Button", format("UnitFrame%d", i), self.GroupFrame, BackdropTemplateMixin and "BackdropTemplate, SecureActionButtonTemplate")
 
---         button:Hide()
+        button:SetWidth(db.width)
+        button:SetHeight(db.height)
+        button:SetBackdrop({ bgFile = "Interface/Tooltips/UI-Tooltip-Background" })
+        button:SetPoint("BOTTOMLEFT", parent, currentUnitOffsetX, currentUnitOffsetY)
+        button.unit = nil
 
---         function button:SetThreatPercent(alpha)
---             button.texture:SetGradientAlpha("VERTICAL", 1, 0, 0, alpha, alpha, 0, 0, 0)
---         end
+        button.texture = button:CreateTexture(nil, "PARENT")
+        button.texture:SetColorTexture(1, 1, 1, 1)
+        button.texture:SetAllPoints(button)
+        button.texture:SetGradientAlpha("VERTICAL", 1, 0, 0, 0, 0, 0, 0, 0)
 
---         function button:SetRole(role)
---             if role == "TANK" then
---                 button.badge:SetTexCoord(.523, .757, 0, 1)
---             elseif role == "HEALER" then
---                 button.badge:SetTexCoord(.265, .492, 0, 1)
---             elseif role == "DAMAGER" then
---                 button.badge:SetTexCoord(.007, .242, 0, 1)
---             else
---                 button.badge:SetTexCoord(.76, 1, 0, 1)
---             end
---         end
+        button.text = button:CreateFontString(nil, "ARTWORK")
+        button.text:SetFont(data.Font, db.font_size)
+        button.text:SetPoint("CENTER", button, "CENTER")
 
---         currentUnitOffsetX = currentUnitOffsetX + (db.width + db.unit_padding)
+        button.badge = button:CreateTexture(nil, "PARENT")
+        button.badge:SetSize(20, 20)
+        button.badge:SetTexture(2202478)
+        button.badge:SetPoint("TOPLEFT", -5, 5)
 
---         if currentUnitOffsetX > maxWidth then
---             currentUnitOffsetX = db.frame_padding
---             currentUnitOffsetY = currentUnitOffsetY + (db.height + db.unit_padding)
---         end
---     end
+        button:Hide()
 
---     self:OnOptionsUpdated()
--- end
+        function button:SetThreatPercent(alpha)
+            button.texture:SetGradientAlpha("VERTICAL", 1, 0, 0, alpha, alpha, 0, 0, 0)
+        end
+
+        function button:SetRole(role)
+            if role == "TANK" then
+                button.badge:SetTexCoord(.523, .757, 0, 1)
+            elseif role == "HEALER" then
+                button.badge:SetTexCoord(.265, .492, 0, 1)
+            elseif role == "DAMAGER" then
+                button.badge:SetTexCoord(.007, .242, 0, 1)
+            else
+                button.badge:SetTexCoord(.76, 1, 0, 1)
+            end
+        end
+
+        currentUnitOffsetX = currentUnitOffsetX + (db.width + db.unit_padding)
+
+        if currentUnitOffsetX > maxWidth then
+            currentUnitOffsetX = db.frame_padding
+            currentUnitOffsetY = currentUnitOffsetY + (db.height + db.unit_padding)
+        end
+    end
+
+    self:OnOptionsUpdated()
+end
+
+function addon:OnOptionsUpdated()
+    sdb:log_debug("OnOptionsUpdated")
+
+    maxWidth = ((db.width + (db.unit_padding)) * unitFrameColumnCount) - db.unit_padding
+    maxHeight = ((db.height + (db.unit_padding)) * unitFrameColumnCount) - db.unit_padding
+
+    local unitCount = sdb:count_table_pairs(groupGuidList)
+    local groupFrameWidth = (db.width + (db.unit_padding)) * unitCount
+    local groupFrameHeight = db.height
+
+    if groupFrameWidth > maxWidth then
+        groupFrameWidth = maxWidth
+        local rows = math.floor(unitCount / unitFrameColumnCount)
+        if rows * unitFrameColumnCount < unitCount then
+            rows = rows + 1
+        end
+        groupFrameHeight = ((groupFrameHeight + db.unit_padding) * rows) - db.unit_padding
+    end
+
+    groupFrameWidth = groupFrameWidth + (db.frame_padding * 2) - db.unit_padding
+    groupFrameHeight = groupFrameHeight + (db.frame_padding * 2)
+
+    self.GroupFrame:SetWidth(groupFrameWidth)
+    self.GroupFrame:SetHeight(groupFrameHeight)
+
+    if (not db.always_show and not inCombat) or not db.enabled then
+        self.GroupFrame:Hide()
+    elseif db.always_show and db.enabled then
+        self.GroupFrame:Show()
+    end
+
+    local offsetX = db.frame_padding
+    local offsetY = db.frame_padding
+
+    for _, child in ipairs({self.GroupFrame:GetChildren()}) do
+        child:SetWidth(db.width)
+        child:SetHeight(db.height)
+        child.text:SetFont(data.Font, db.font_size)
+
+        local unitName = child:GetName()
+
+        if (unitName == "player" and db.include_player) or unitName ~= "player" then
+            child.text:SetFont(data.Font, db.font_size)
+            child:SetPoint("BOTTOMLEFT", self.GroupFrame, offsetX, offsetY)
+
+            offsetX = offsetX + (db.width + db.unit_padding)
+
+            if offsetX > maxWidth then
+                offsetX = db.frame_padding
+                offsetY = offsetY + (db.height + db.unit_padding)
+            end
+        end
+    end
+end
+
+function addon:UpdatePlayerSpec()
+    sdb:log_debug("UpdatePlayerSpec")
+
+    specIndex = GetSpecialization()
+    spec = specIndex and select(2, GetSpecializationInfo(specIndex)) or "None"
+
+    if data.ClassData[class] and data.ClassData[class]["spec"] == spec then
+        tauntSpellId, tauntSpellName = data.ClassData[class]["tauntSpellId"], data.ClassData[class]["tauntSpellName"]
+    end
+end
+
+function addon:UpdatePlayerGroupState()
+    sdb:log_debug("UpdatePlayerGroupState")
+
+    inParty = IsInGroup()
+    inRaid = IsInRaid()
+    playerRole = UnitGroupRolesAssigned("player")
+end
+
+function addon:UpdateGroupGuidList()
+    sdb:log_debug("UpdateGroupGuidList")
+
+    wipe(groupGuidList)
+
+    if not inRaid and include_player then
+        groupGuidList["player"] = {
+            guid = UnitGUID("player"),
+            name = UnitName("player"),
+            target = "target"
+        }
+    end
+
+    if inRaid then
+        for i = 1, GetNumGroupMembers() do
+            local unit = format("raid%d", i)
+            local target = format("raid%dtarget", i)
+
+            if UnitExists(unit) then
+                groupGuidList[unit] = {
+                    guid = UnitGUID(unit),
+                    name = UnitName(unit),
+                    target = target
+                }
+            end
+        end
+    elseif inParty then
+        for i = 1, GetNumSubgroupMembers() do
+            local unit = format("party%d", i)
+            local target = format("party%dtarget", i)
+
+            if UnitExists(unit) then
+                groupGuidList[unit] = {
+                    guid = UnitGUID(unit),
+                    name = UnitName(unit),
+                    target = target
+                }
+            end
+        end
+    elseif isTesting then
+        for i = 1, maxUnitFrames - 1 do -- minus one here to account for added player unit frame
+            local unit = "player"
+            local target = "target"
+
+            groupGuidList[unit .. i] = {
+                guid = UnitGUID("player"),
+                name = UnitName("player"),
+                target = target
+            }
+        end
+    end
+end
+
+function addon:UpdateGroupFrameUnits()
+    sdb:log_debug("UpdateGroupFrameUnits")
+
+    if groupGuidList then
+        local unitCount = sdb:count_table_pairs(groupGuidList)
+        sdb:log_debug("unitCount:", unitCount)
+
+        local groupFrameWidth = (db.width + (db.unit_padding)) * unitCount
+        local groupFrameHeight = db.height
+
+        if groupFrameWidth > maxWidth then
+            groupFrameWidth = maxWidth
+            local rows = math.floor(unitCount / unitFrameColumnCount)
+            if rows * unitFrameColumnCount < unitCount then
+                rows = rows + 1
+            end
+            groupFrameHeight = ((groupFrameHeight + db.unit_padding) * rows) - db.unit_padding
+        end
+
+        groupFrameWidth = groupFrameWidth + (db.frame_padding * 2) - db.unit_padding
+        groupFrameHeight = groupFrameHeight + (db.frame_padding * 2)
+
+        self.GroupFrame:SetWidth(groupFrameWidth)
+        self.GroupFrame:SetHeight(groupFrameHeight)
+
+        self.GroupFrame:ResetUnitFrames()
+
+        local unitFrameIndex = 1
+        local unitFrames = self.GroupFrame:GetChildren()
+
+        for unit, data in pairs(groupGuidList) do
+            local unitFrame = self.GroupFrame:GetUnitFrame(format("UnitFrame%d", unitFrameIndex))
+            local unitName = data["name"]
+
+            if unitFrame then
+                unitFrame:SetBackdropColor(0, 0, 0, 1)
+
+                if unitName ~= UnitName("player") then
+                    if playerRole == "TANK" and tauntSpellName then
+                        unitFrame:SetAttribute("type", "spell")
+                        unitFrame:SetAttribute("spell", tauntSpellName)
+                        unitFrame:SetAttribute("unit", data["target"])
+                    else
+                        unitFrame:SetAttribute("type", "assist")
+                        unitFrame:SetAttribute("unit", data["target"])
+                    end
+                end
+
+                unitFrame.unit = unit
+                unitFrame:SetRole(UnitGroupRolesAssigned(unit))
+                unitFrame.text:SetText(UnitName(unit))
+                unitFrame:Show()
+            else
+                sdb:log_debug("UpdateGroupFrameUnits nil unitFrame for unit:", unit)
+            end
+
+            unitFrameIndex = unitFrameIndex + 1
+        end
+    end
+end
+
+function addon:GetGroupUnit(unit)
+    sdb:log_debug("GetGroupUnit: ", unit)
+
+    if groupGuidList then
+        if groupGuidList[unit] or groupGuidList[UnitGUID(unit)] then
+            return unit
+        else
+            for u, d in pairs(groupGuidList) do
+                if d["name"] == unit or d["name"] == UnitName(unit) then
+                    return u
+                end
+            end
+        end
+    end
+
+    return nil
+end
+
+function addon:InGroup(unit)
+    sdb:log_debug("InGroup: ", unit)
+
+    if self:GetGroupUnit(unit) then
+        return true
+    else
+        return false
+    end
+end
+
+function addon:UpdateUnitFramesThreat()
+    sdb:log_debug("UpdateUnitFramesThreat")
+
+    if not inRaid and db.include_player then
+        local playerIsTanking, playerThreatStatus, playerThreatPct, playerRawThreatPct, playerThreatValue =
+            UnitDetailedThreatSituation("player", "target")
+
+        if playerThreatPct then
+            self.GroupFrame:UpdateThreatForUnit("player", (playerThreatPct / threatPercentDivisor))
+        end
+    end
+
+    local groupGuidList = groupGuidList
+
+    if groupGuidList then
+        for unit, data in pairs(groupGuidList) do
+            local isTanking, threatStatus, threatPct, rawThreatPct, threatValue =
+                UnitDetailedThreatSituation(unit, data["target"])
+
+            if threatPct then
+                self.GroupFrame:UpdateThreatForUnit(unit, (threatPct / threatPercentDivisor))
+            end
+
+            -- local threatSituation = UnitThreatSituation(unit) or 0
+
+            -- if threatSituation > 0 then
+            --     if not self:InGroup(data["target"]) then
+            --         -- target is not in group, check threat
+            --         local isTanking, threatStatus, threatPct, rawThreatPct, threatValue =
+            --             UnitDetailedThreatSituation(unit, data["target"])
+
+            --         if threatPct then
+            --             self.GroupFrame:UpdateThreatForUnit(unit, (threatPct / threatPercentDivisor))
+            --         end
+            --     else
+            --         local unitTargetTarget = data["target"] .. "target"
+
+            --         if not self:InGroup(unitTargetTarget) then
+            --             local isTanking, threatStatus, threatPct, rawThreatPct, threatValue =
+            --                 UnitDetailedThreatSituation(unit, unitTargetTarget)
+
+            --             if threatPct then
+            --                 self.GroupFrame:UpdateThreatForUnit(unit, (threatPct / threatPercentDivisor))
+            --             end
+            --         else
+            --             self.GroupFrame:UpdateThreatForUnit(unit, (1 / threatSituation))
+            --         end
+            --     end
+            -- else
+            --     self.GroupFrame:UpdateThreatForUnit(unit, 0)
+            -- end
+        end
+    end
+end
 
 -- event functions:
 function addon:ADDON_LOADED(addOnName)
